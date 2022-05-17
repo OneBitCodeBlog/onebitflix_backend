1859f47624ad5582494f3d42fb783cf66f04ce3b
Isaac Pontes <isaaclfp@outlook.com>
2022-05-17 20:55:33 -0300

    Configurando o Painel do AdminJS

---
+++ src/adminjs/index.ts
@@ -0,0 +1,34 @@
+import AdminJS from "adminjs";
+import AdminJSExpress from "@adminjs/express"
+import AdminJSSequelize from "@adminjs/sequelize"
+import { sequelize } from "../database";
+
+AdminJS.registerAdapter(AdminJSSequelize)
+
+export const adminJs = new AdminJS({
+    databases: [sequelize],
+    rootPath: "/admin",
+    branding: {
+        companyName: 'OneBitFlix',
+        logo: '/onebitflix.svg',
+        theme: {
+            colors: {
+                primary100: '#ff0043',
+                primary80: '#ff1a57',
+                primary60: '#ff3369',
+                primary40: '#ff4d7c',
+                primary20: '#ff668f',
+                grey100: '#151515',
+                grey80: '#333333',
+                grey60: '#4d4d4d',
+                grey40: '#666666',
+                grey20: '#dddddd',
+                filterBg: '#333333',
+                accent: '#151515',
+                hoverBg: '#151515',
+            }
+        }
+    }
+})
+
+export const adminJsRouter = AdminJSExpress.buildRouter(adminJs)
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import express from "express"
+import { adminJs, adminJsRouter } from "./adminjs"
 import { sequelize } from "./database"
 
 const app = express()
+
+app.use(express.static('public'))
+
+app.use(adminJs.options.rootPath, adminJsRouter)
 
 const PORT = process.env.PORT || 3000
 
