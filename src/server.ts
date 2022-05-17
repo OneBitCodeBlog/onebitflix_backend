e069a7f2182a585d9336162d5c8a33b78c4b4df7
Isaac Pontes <isaaclfp@outlook.com>
2022-05-17 19:59:22 -0300

    Iniciando o projeto

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from "express"
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server started successfuly at port ${PORT}`)
+})
