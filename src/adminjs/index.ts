6aebe71a34d04e5a0f348b70b06657b1d5239248
Isaac Pontes <isaaclfp@outlook.com>
2022-05-19 21:01:00 -0300

    Autenticação e tela de login

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -3,6 +3,8 @@ import AdminJSExpress from "@adminjs/express"
 import AdminJSSequelize from "@adminjs/sequelize"
 import { sequelize } from "../database";
 import { adminJsResources } from "./resources";
+import { User } from "../models";
+import bcrypt from "bcrypt"
 
 AdminJS.registerAdapter(AdminJSSequelize)
 
@@ -33,4 +35,22 @@ export const adminJs = new AdminJS({
     }
 })
 
-export const adminJsRouter = AdminJSExpress.buildRouter(adminJs)
+export const adminJsRouter = AdminJSExpress.buildAuthenticatedRouter(adminJs, {
+    authenticate: async (email, password) => {
+        const user = await User.findOne({ where: { email } })
+
+        if (user && user.role === 'admin') {
+            const matched = await bcrypt.compare(password, user.password)
+
+            if (matched) {
+                return user
+            }
+        }
+
+        return false
+    },
+    cookiePassword: 'senha-de-cookie'
+}, null, {
+    resave: false,
+    saveUninitialized: false
+})
