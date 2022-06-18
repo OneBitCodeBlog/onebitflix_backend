83b711341f97c2e3121168888a5855f0f3664d0e
Isaac Pontes <isaaclfp@outlook.com>
2022-06-18 20:55:31 -0300

    Protegendo os vídeos

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -17,14 +17,35 @@ export function ensureAuth(req: AuthenticatedRequest, res: Response, next: NextF
 
   const token = authorizationHeader.replace(/Bearer /, '')
 
-  jwtService.verifyToken(token, (err, decoded) => {
+  jwtService.verifyToken(token, async (err, decoded) => {
     if (err || typeof decoded === 'undefined') return res.status(401).json({
       message: 'Não autorizado: token inválido.'
     })
 
-    userService.findByEmail((decoded as JwtPayload).email).then(user => {
-      req.user = user
-      next()
+    const user = await userService.findByEmail((decoded as JwtPayload).email)
+    req.user = user
+    next()
+  })
+}
+
+export function ensureAuthViaQuery(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+  const { token } = req.query
+
+  if (!token) return res.status(401).json({
+    message: 'Não autorizado: nenhum token foi encontrado.'
+  })
+
+  if (typeof token !== 'string') return res.status(400).json({
+    message: 'O parâmetro token deve ser do tipo string'
+  })
+
+  jwtService.verifyToken(token, async (err, decoded) => {
+    if (err || typeof decoded === 'undefined') return res.status(401).json({
+      message: 'Não autorizado: token inválido.'
     })
+
+    const user = await userService.findByEmail((decoded as JwtPayload).email)
+    req.user = user
+    next()
   })
 }
--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import { authController } from './controllers/authController'
 import { categoriesController } from './controllers/categoriesController'
 import { coursesController } from './controllers/coursesController'
 import { episodesController } from './controllers/episodesController'
-import { ensureAuth } from './middlewares/auth'
+import { ensureAuth, ensureAuthViaQuery } from './middlewares/auth'
 
 const router = express.Router()
 
@@ -18,6 +18,6 @@ router.get('/courses/newest', coursesController.newest)
 router.get('/courses/search', ensureAuth, coursesController.search)
 router.get('/courses/:id', ensureAuth, coursesController.show)
 
-router.get('/episodes/stream', episodesController.stream)
+router.get('/episodes/stream', ensureAuthViaQuery, episodesController.stream)
 
 export { router }
