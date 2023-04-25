856cf764cc51feced94d6a144bda821a466e3d15
Isaac Pontes <isaaclfp@outlook.com>
2023-04-25 12:10:56 -0300

    fix: add environment variable for adminjs cookie password

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -3,3 +3,5 @@ import * as env from "env-var"
 export const DATABASE_URL = env.get("DATABASE_URL").required().asString()
 
 export const JWT_KEY = env.get("JWT_KEY").required().asString()
+
+export const ADMINJS_COOKIE_PASSWORD = env.get("ADMINJS_COOKIE_PASSWORD").required().asString()
